37283c011f9bcf7be5338cad176ff56645aa6852
James Fairbanks <james@ionicsecurity.com>
2016-04-05 10:46:20 -0400

    lufact and cg based katz centrality
    with tests

--- src/Katz.jl
+++ src/Katz.jl
@@ -1,5 +1,28 @@
 module Katz
+using LightGraphs
+using GraphMatrices
+using IterativeSolvers
+export katz, stdbasis, ikatz, ρ
 
-# package code goes here
+function katz(A, a, v)
+    F = lufact(I-a*A)
+    return A*(F\v)
+end
+
+function ikatz(A::KatzAdjacency, b::AbstractVector, tol::Real=1e-8, maxiter::Integer=100)
+    x, convhist = cg(A, b, tol=tol, maxiter=maxiter)
+    return x, convhist
+end
+
+function ρ(convhist::ConvergenceHistory)
+    rs = @show convhist.residuals
+    return mean([rs[i+1]/rs[i] for i in 1:length(rs)-1])
+end
+
+function stdbasis(n,i)
+    x = ones(n)
+    x[i] + 1
+    return x
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,44 @@
 using Katz
 using Base.Test
+using LightGraphs
+using GraphMatrices
+import GraphMatrices: KatzAdjacency
+a = 0.15
+g = LightGraphs.Datasets.BullGraph()
+function test1(g, a::Number)
+    n = nv(g)
+    A = sparse(CombinatorialAdjacency(g))
+    v = stdbasis(n, 1)
+    k = katz(A, a, v)
+    k2 = katz(A, a, ones(n))
+    @test_approx_eq_eps k2 k 1e-5
+end
 
-# write your own tests here
-@test 1 == 1
+function test_iterative(g, a)
+    K = KatzAdjacency(CombinatorialAdjacency(g), a)
+    n = nv(g)
+    y = K*ones(n)
+    z, convhist = ikatz(K,ones(n))
+    x = K.A*z
+    @show convhist.isconverged
+    x_lu = katz(sparse(K.A), a, ones(n))
+    @test norm(x_lu - x) < 1e-6
+    @show ρ(convhist)
+end
+datasets = [:BullGraph,
+            :TutteGraph,
+            :DesarguesGraph]
+for a in [0.1,0.01,0.001]
+for s in datasets
+    g = eval(:(LightGraphs.Datasets.$s()))
+    println("test1 on $s, $a")
+    test1(g, a)
+    test_iterative(g,a)
+end
+end
+
+for s in ["Newman/football"]
+    g = MDGraph(s,:read)
+    test1(g, α)
+    test_iterative(g,α)
+end
